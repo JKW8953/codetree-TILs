25f9bde3b86be522808bb737d5c08326d17d0fbc
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-11 01:45:05 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    bool flag = true;
+        stack<char> st;
+        string str;
+        cin >> str;
+        for (int i = 0; i < str.size(); ++i)
+        {
+            if (str[i] == '(')
+                st.push('(');
+            else
+            {
+                if (st.empty() == false)
+                    st.pop();
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if ((flag == false) || (st.empty() == false))
+            cout << "No" << '\n';
+        else
+            cout << "Yes" << '\n';
+    return 0;
+}
