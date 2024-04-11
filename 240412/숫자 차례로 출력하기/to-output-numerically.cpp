ec2218a7cd26461e53284b80005ebd70a340bc86
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-12 01:16:19 +0900

    Title: 숫자 차례로 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void f1(int N){
+    if(N==0)
+        return;
+    f1(N-1);
+    cout << N << " ";
+}
+
+void f2(int N){
+    if(N==0)
+        return;
+    cout << N << " ";
+    f2(N-1);
+}
+
+int main() {
+    int N;
+    cin >> N;
+    f1(N);
+    cout << '\n';
+    f2(N);
+    return 0;
+}
