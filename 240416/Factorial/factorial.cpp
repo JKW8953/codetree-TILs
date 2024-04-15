32a7aa5901436083b065a0820537f9fa1e8706e9
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-16 00:42:17 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240416/Factorial/factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int Factorial(int N){
+    if(N==0) return 1;
+
+    return N * Factorial(N-1);
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << Factorial(N);
+    return 0;
+}
