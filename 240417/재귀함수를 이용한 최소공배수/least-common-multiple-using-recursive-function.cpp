1c8b3563f68c7cba9888e380ecd01637175838b1
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-17 14:00:55 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240417/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240417/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -18,10 +18,8 @@ int main() {
     for(int i=0;i<n;++i)
         cin >> arr[i];
 
-    int gcd = 0, lcd = 0;
-    gcd = GCD(arr[0], arr[1]);
-    lcd = (arr[0]*arr[1]) / gcd;
-    for(int i=1;i<n-1;++i){
+    int gcd = arr[0], lcd = arr[0];
+    for(int i=0;i<n-1;++i){
         gcd = GCD(lcd, arr[i+1]);
         lcd = (lcd * arr[i+1]) / gcd;
     }
