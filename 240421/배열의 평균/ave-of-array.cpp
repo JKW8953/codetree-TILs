c99a4f1fd0692a7e7de2ed8ea33f9490bf26f67e
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-21 20:44:31 +0900

    Title: 배열의 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240421/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	int arr[2][4];
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+    cout << fixed;
+    cout << setprecision(1);
+
+	for (int i = 0; i < 2; i++) {
+        double sum = 0;
+		for (int j = 0; j < 4; j++) {
+            sum += arr[i][j];
+		}
+        cout << sum / 4 << " ";
+	}
+    cout << '\n';
+
+    for (int j = 0; j < 4; j++) {
+        double sum = 0;
+		for (int i = 0; i < 2; i++) {
+            sum += arr[i][j];
+		}
+        cout << sum / 2 << " ";
+	}
+    cout << '\n';
+
+    double sum = 0;
+    for (int j = 0; j < 4; j++) {
+		for (int i = 0; i < 2; i++) {
+            sum += arr[i][j];
+		}
+	}
+    cout << sum / 8 << " ";
+
+	return 0;
+
+}
