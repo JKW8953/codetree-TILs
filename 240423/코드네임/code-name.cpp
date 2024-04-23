b7ebf43d20ed4c976d426c768452cef54a6f5875
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-23 21:56:11 +0900

    Title: 코드네임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240423/코드네임/code-name.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Agent {
+public:
+	string codename;
+	int score;
+
+	Agent(string codename, int score) {
+		this->codename = codename;
+		this->score = score;
+	}
+};
+
+int main() {
+	vector<Agent> vec;
+	
+	for (int i = 0; i < 5; ++i) {
+		string codename;
+		int score;
+		cin >> codename >> score;
+		vec.push_back(Agent(codename, score));
+	}
+
+	int idx = 0;
+	for (int i = 1; i < 5; ++i) {
+		if (vec[i].score > vec[i - 1].score)
+			idx == i;
+	}
+
+	cout << vec[idx].codename << " " << vec[idx].score;
+	return 0;
+}
