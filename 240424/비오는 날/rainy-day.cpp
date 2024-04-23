8b04758b7cf4f0ad10bed7554e68c4295f161dbd
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-24 00:36:29 +0900

    Title: 비오는 날, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240424/비오는 날/rainy-day.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Data {
+public:
+	string date, day, weather;
+
+	Data(string date, string day, string weather) {
+		this->date = date;
+		this->day = day;
+		this->weather = weather;
+	}
+};
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	Data result("", "", "Rain");
+
+	for (int i = 0; i < n; ++i) {
+		string date, day, weather;
+		cin >> date >> day >> weather;
+		if (weather == "Rain")
+			if (date <= result.date) {
+				result.date = date;
+				result.day = day;
+			}
+	}
+
+	cout << result.date << " " << result.day << " " << result.weather;
+
+	return 0;
+}
