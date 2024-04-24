38eb28dc3fddef2de031a26b77f3e0f674ad3bd0
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-24 17:16:12 +0900

    Title: 원점으로부터의 거리, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240424/원점으로부터의 거리/distance-from-origin.cpp
+++ 240424/원점으로부터의 거리/distance-from-origin.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 
 bool cmp(pair<int, int> p1, pair<int, int> p2) {
-	return p1.second < p2.second;
+	if (p1.second != p2.second;)
+		return p1.second < p2.second;
+	return p1.first < p2.first;
 }
 
 int main()
