fd32fb2693ae851953e123e0c90e96832e0c5920
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-24 17:12:39 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240424/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(pair<int, int> p1, pair<int, int> p2) {
+	return p1.second < p2.second;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<pair<int, int>> vec;
+	
+	for (int i = 0; i < n; ++i) {
+		int x, y;
+		cin >> x >> y;
+		vec.push_back(make_pair(i + 1, abs(x) + abs(y)));
+	}
+
+	sort(vec.begin(), vec.begin() + n, cmp);
+
+	for (int i = 0; i < n; ++i)
+		cout << vec[i].first << '\n';
+}
