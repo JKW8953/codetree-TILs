6be5de612f281f3a0e89c83ae9530a5366d78a3e
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-04-27 19:09:37 +0900

    Title: 줄 세우기 2, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240427/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Person {
+public:
+	int idx, height, weight;
+	Person(int idx, int weight, int height) {
+		this->idx = idx;
+        this->weight = weight;
+		this->height = height;
+	}
+	Person() {};
+};
+
+bool cmp(Person a, Person b) {
+	if(a.height != b.height)
+        return a.height < b.height;
+    return a.weight > b.weight;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	Person people[1000];
+
+	for (int idx = 1; idx <= n; ++idx) {
+		int weight, height;
+		cin >> height >> weight;
+		people[idx - 1] = Person(idx, weight, height);
+	}
+
+	sort(people, people + n, cmp);
+
+	for (int i = 0; i < n; ++i)
+		cout << people[i].height << " " << people[i].weight << " " << people[i].idx << '\n';
+}
