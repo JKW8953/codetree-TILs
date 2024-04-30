02de2f6e94ccd15c8034ee3bb9fc6bb13ef05022
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-01 07:38:46 +0900

    Title: 가장 큰 수 고르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int arr[10];
+    for (int i = 0; i < 10; ++i)
+        cin >> arr[i];
+    
+    int max = INT_MIN;
+
+    for (int i = 0; i < 10; ++i)
+        if (arr[i] > max) max = arr[i];
+    
+    cout << max;
+    return 0;
+}
