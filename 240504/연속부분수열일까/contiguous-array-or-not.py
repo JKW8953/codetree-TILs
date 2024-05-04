4d63b3f0a74eeef311b52354fa6d4e362d7c7a6c
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-04 17:37:30 +0900

    Title: 연속부분수열일까, Time: 228ms, Memory: 24MB, Status: Passed - CodeTree

--- 240504/연속부분수열일까/contiguous-array-or-not.py
+++ 240504/연속부분수열일까/contiguous-array-or-not.py
@@ -7,6 +7,8 @@ B = list(map(int, input().split()))
 for i in range (len(A)):
     if A[i] == B[0]:
         for j in range (len(B)):
+            if i+j >= len(A):
+                break
             if A[i+j] != B[j]:
                 break
             if j == len(B) - 1:
