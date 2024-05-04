a077ae10a5ef66dfb4edcc4a6f7d84fd179c4991
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-04 17:52:10 +0900

    Title: 주사위 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240504/주사위 놀이/play-with-dice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int arr[7] = {};
+    for (int i = 0; i < 10; ++i){
+        int num; cin >> num;
+        arr[num]++;
+    }
+
+    for (int i = 1; i < 7; ++i)
+        cout << i << " - " << arr[i] << '\n';
+        
+    return 0;
+}
