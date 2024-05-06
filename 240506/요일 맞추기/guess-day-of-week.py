93597b6533f151fd343c0e47117e0d137e9b4b51
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-06 23:28:37 +0900

    Title: 요일 맞추기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240506/요일 맞추기/guess-day-of-week.py
+++ 240506/요일 맞추기/guess-day-of-week.py
@@ -1,7 +1,7 @@
-month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day2 = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 m1, d1, m2, d2 = map(int, input().split())
-tmp1 = sum(month[0:m1]) + d1
-tmp2 = sum(month[0:m2]) + d2
+tmp1 = sum(month[1:m1]) + d1
+tmp2 = sum(month[1:m2]) + d2
 res = (tmp2 - tmp1) % 7
-print(day2[res])
+print(day[res])
