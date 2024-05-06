642cc29cdc845763b2d2017359b1277b276cb5ef
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-06 16:38:14 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240506/2개씩 그룹짓기/group-of-pairs.py
+++ 240506/2개씩 그룹짓기/group-of-pairs.py
@@ -5,7 +5,7 @@ arr.sort()
 
 maximum = 0
 for i in range(int(n)):
-    min_maximum = arr[i] + arr[2*n-i-1]
+    maximum = arr[i] + arr[2*n-i-1]
     if maximum > min_maximum:
         min_maximum = maximum
     
