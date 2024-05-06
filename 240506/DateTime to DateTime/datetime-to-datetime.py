66203a38964c22b72b3afbb906bdc90fef71f4cd
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-06 22:54:51 +0900

    Title: DateTime to DateTime, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240506/DateTime to DateTime/datetime-to-datetime.py
+++ 240506/DateTime to DateTime/datetime-to-datetime.py
@@ -1,6 +1,6 @@
 a, b, c = map(int, input().split())
 
-if b<11 or (b==11 and c<11):
+if a<11 or (a==11 and b<11) or (a==11 and b==11 and c<11):
     print(-1)
 else:
     print(1440*(a-11)+60*(b-11)+(c-11))
