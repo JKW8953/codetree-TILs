d0cc6244b55ad84b9e0cb713909665ce04e80f28
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-06 16:01:45 +0900

    Title: 대각선으로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240506/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int arr[100][100] = {};
+
+    int cnt = 1;
+    for (int i = 0; i < m; ++i){
+        for (int row = 0, col = i; row < n && col >= 0; ++row, --col){
+            arr[row][col] = cnt++;
+        }
+    }
+
+    for (int i = 1; i < n; ++i){
+        for (int row = i, col = m - 1; row < n && col >= 0; ++row, --col){
+            arr[row][col] = cnt++;
+        }
+    }
+
+    for (int row = 0; row < m; ++row){
+        for (int col = 0; col < n; ++col)
+            cout << arr[row][col] << ' ';
+        cout << '\n';
+    }
+    return 0;
+}
