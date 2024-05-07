54cf5c6e1f6a16d893174a4c2409373fa9d81b52
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-07 15:51:32 +0900

    Title: 중앙값 계산 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/중앙값 계산 2/get-median-2.py
+++ 240507/중앙값 계산 2/get-median-2.py
@@ -1,7 +1,7 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-for i in range(n):
-    if i % 2 == 0: 
-        new_arr = sorted(arr)
+for i in range(1, n+1):
+    if i % 2 != 0: 
+        new_arr = sorted(arr[:i + 1])
         print(new_arr[i // 2], end=' ')
