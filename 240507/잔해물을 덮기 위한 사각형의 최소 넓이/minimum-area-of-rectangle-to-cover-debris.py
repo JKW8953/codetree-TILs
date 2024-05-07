7b2945bb771350acd071c96de93a2ed0712e1f1a
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-07 15:35:33 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240507/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,27 @@
+OFFSET = 1000
+
+arr = [
+    [0 for _ in range(2001)] 
+    for _ in range(2001)
+]
+
+for num in range(1, 3):
+    x1, y1, x2, y2 = map(lambda x: int(x) + OFFSET, input().split())
+    for j in range(y1, y2):
+        for i in range(x1, x2):
+            arr[i][j] = num
+
+count = 0
+
+row_val = []
+col_val = []
+
+for row in range(2001):
+    for col in range(2001):
+        if arr[row][col] == 1:
+            row_val.append(row)
+            col_val.append(col)
+
+S = (max(row_val) - min(row_val) + 1) * (max(col_val) - min(col_val) + 1)
+
+print(S)
