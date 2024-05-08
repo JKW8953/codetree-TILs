64b1edd3d9db5ff1bf3b9957d779a73235c5eef9
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-09 04:08:49 +0900

    Title: 키를 기준으로 정렬, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240509/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, h, w):
+        self.name, self.h, self.w = name, h, w
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, h, w = tuple(input().split())
+    students.append(Student(name, int(h), int(w)))
+
+students.sort(lambda s: s.h)
+
+for elem in students:
+    print(elem.name, elem.h, elem.w)
