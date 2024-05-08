913f66f8258e72c34b4290a3214ba82da12b0753
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-08 14:40:58 +0900

    Title: 폭탄 해제, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240508/폭탄 해제/bomb-removal.py
@@ -0,0 +1,13 @@
+class Bomb:
+    def __init__(self, code, color, second):
+        self.code = code
+        self.color = color
+        self.second = second
+
+code, color, second = input().split()
+second = int(second)
+
+bomb1 = Bomb(code, color, second)
+print("code :", bomb1.code)
+print("color :", bomb1.color)
+print("second :", bomb1.second)
