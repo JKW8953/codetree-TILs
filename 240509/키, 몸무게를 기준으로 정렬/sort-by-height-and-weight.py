a8cff21cf88d7b06566cb4287ff8664b63519682
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-09 09:35:36 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240509/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, h, w):
+        self.name, self.h, self.w = name, h, w
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, h, w = input().split()
+    students.append(Student(name, int(h), int(w)))
+
+students.sort(key = lambda s: (s.h, -s.w))
+
+for elem in students:
+    print(elem.name, elem.h, elem.w)
