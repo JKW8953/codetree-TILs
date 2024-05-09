b36f03f18688200dfb81e6f05dfcb6dcccdf4c70
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-10 08:54:16 +0900

    Title: 줄 세우기 2, Time: 210ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240510/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, h, w, i):
+        self.h, self.w, self.i = h, w, i
+
+n = int(input())
+students = []
+for i in range(1, n+1):
+    h, w = map(int, input().split())
+    students.append(Student(h, w, i))
+
+students.sort(key = lambda s: (s.h, -s.w))
+
+for elem in students:
+    print(elem.h, elem.w, elem.i)
