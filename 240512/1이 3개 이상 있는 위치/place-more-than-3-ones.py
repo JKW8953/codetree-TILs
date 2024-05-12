28a6f65695f0a680fb5d3a661e9b76ad7ad2854e
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-12 15:17:19 +0900

    Title: 1이 3개 이상 있는 위치, Time: 172ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240512/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def check(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
+            cnt += 1
+    if cnt >= 3:
+        return True
+
+ans = 0
+for x in range(n):
+    for y in range(n):
+        if check(x, y):
+            ans += 1
+
+print(ans)
