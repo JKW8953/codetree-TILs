7bf475fc2f53e9b15a8d0e8f0943e614c0a12d5a
JKW8953 <164450033+JKW8953@users.noreply.github.com>
2024-05-13 02:04:53 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240513/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    int cnt = 0;
+    for (int i = 0; i < str.length(); ++i){
+        if (str[i] == '(')
+            for (int j = i + 1; j < str.length(); ++j)
+                if (str[j] == ')') cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
